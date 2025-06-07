3ae5665c1ac1d068e587417fa3ee5f8aef977c99
mokiser <104251490+mokiser@users.noreply.github.com>
2025-06-07 13:17:45 +0300

    Update toolchain-rpi.cmake

--- toolchain-rpi.cmake
+++ toolchain-rpi.cmake
@@ -4,7 +4,11 @@ set(CMAKE_SYSTEM_PROCESSOR arm)
 set(CMAKE_C_COMPILER /usr/bin/arm-linux-gnueabihf-gcc)
 set(CMAKE_CXX_COMPILER /usr/bin/arm-linux-gnueabihf-g++)
 
-set(OPENSSL_ROOT_DIR /usr/arm-linux-gnueabihf)
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -static")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -static")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static")
+
+set(CMAKE_FIND_ROOT_PATH /usr/arm-linux-gnueabihf)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
